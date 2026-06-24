5b004bce4f0d9d7de60c066397571decf6fb8397
unknown <surathmdr@gmail.com>
2026-06-24 20:46:44 +0545

    new file

---
+++ main.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  const navLinks = document.getElementById("navLinks");
+  navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
+}
+
+// Example: simple alert when page loads
+window.onload = () => {
+  console.log("Welcome to Surath Manandhar's Portfolio!");
+};
